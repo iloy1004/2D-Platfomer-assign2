81d32966fac37412012c9aaac1a49e2aee9db7c6
iloy1004 <iloy1004ca@gmail.com>
2016-02-23 15:00:08 -0500

    Set up
    Set up initiate setting with platform and player

---
+++ AlienInEarth/Assets/_Scripts/PlayerController.cs
@@ -0,0 +1,67 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+
+    // PRIVATE Instance variables
+    private Animator _animator;
+    private float _move;
+    private float _jump;
+    private bool _facingRight;
+    private Transform _transform;
+
+    // Use this for initialization
+    void Start()
+    {
+        this._animator = gameObject.GetComponent<Animator>();
+        this._move = 0f;
+        this._jump = 0f;
+        this._facingRight = true;
+        this._transform = gameObject.GetComponent<Transform>();
+        //set default
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        this._move = Input.GetAxis("Horizontal");
+        this._jump = Input.GetAxis("Vertical");
+
+        if (this._move != 0)
+        {
+            if (this._move > 0)
+            {
+                this._facingRight = true;
+            }
+            if (this._move < 0)
+            {
+                this._facingRight = false;
+            }
+            this._animator.SetInteger("Anim_state", 1);
+        }
+        else
+        {
+            this._animator.SetInteger("Anim_state", 0);
+        }
+
+        if (this._jump > 0)
+        {
+            //call the jump animation
+            this._animator.SetInteger("Anim_state", 2);
+        }
+
+        this._flip();
+    }
+
+    private void _flip()
+    {
+        if (this._facingRight)
+        {
+            this._transform.localScale = new Vector2(1, 1);
+        }
+        else
+        {
+            this._transform.localScale = new Vector2(-1, 1);
+        }
+    }
+}
