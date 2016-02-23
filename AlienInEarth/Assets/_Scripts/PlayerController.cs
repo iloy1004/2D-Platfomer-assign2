7fc9635977cbe570290b34de86c1c690889ac458
iloy1004 <iloy1004ca@gmail.com>
2016-02-23 17:46:03 -0500

    Add movement
    Movement

--- AlienInEarth/Assets/_Scripts/PlayerController.cs
+++ AlienInEarth/Assets/_Scripts/PlayerController.cs
@@ -1,7 +1,29 @@
 ﻿using UnityEngine;
 using System.Collections;
 
+//Velocity range utility class
+[System.Serializable]
+public class VelocityRange
+{
+    //public instance variables
+    public float minimum;
+    public float maximum;
+
+    // Constructor
+    public VelocityRange(float minimum, float maximum)
+    {
+        this.minimum = minimum;
+        this.maximum = maximum;
+    }
+}
+
 public class PlayerController : MonoBehaviour {
+
+    //Public variables
+    public VelocityRange velocityRange;
+    public float moveForce;
+    public float jumpForce;
+    public Transform groundCheck;
 
     // PRIVATE Instance variables
     private Animator _animator;
@@ -9,48 +31,101 @@ public class PlayerController : MonoBehaviour {
     private float _jump;
     private bool _facingRight;
     private Transform _transform;
+    private Rigidbody2D _rigidBody2d;
+    private bool _isGrounded;
 
     // Use this for initialization
     void Start()
     {
+        //Initialize public instance variables
+        this.velocityRange = new VelocityRange(300f, 800f);
+        
+
+        //set private instance variables
         this._animator = gameObject.GetComponent<Animator>();
         this._move = 0f;
         this._jump = 0f;
         this._facingRight = true;
         this._transform = gameObject.GetComponent<Transform>();
-        //set default
+        this._rigidBody2d = gameObject.GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
-        this._move = Input.GetAxis("Horizontal");
-        this._jump = Input.GetAxis("Vertical");
+        this._isGrounded = Physics2D.Linecast(
+                            this._transform.position, 
+                            this.groundCheck.position, 
+                            1 << LayerMask.NameToLayer("Ground"));
 
-        if (this._move != 0)
+        
+
+        float forceX = 0f;
+        float forceY = 0f;
+
+        //get absolute value of velocity for game object
+        float absVelX = Mathf.Abs(this._rigidBody2d.velocity.x);
+        float absVelY = Mathf.Abs(this._rigidBody2d.velocity.y);
+
+        Debug.Log(this._isGrounded);
+
+        if (this._isGrounded)
         {
-            if (this._move > 0)
+            //get a number between -1 to 1 for both Horizontal and Vertical Axes
+            this._move = Input.GetAxis("Horizontal");
+            this._jump = Input.GetAxis("Vertical");
+
+            if (this._move != 0)
             {
-                this._facingRight = true;
+                if (this._move > 0)
+                {
+                    if(absVelX < this.velocityRange.maximum)
+                    {
+                        forceX = this.moveForce;
+                    }
+                    this._facingRight = true;
+                    this._flip();
+
+                    //move force
+                }
+                if (this._move < 0)
+                {
+                    if (absVelX < this.velocityRange.maximum)
+                    {
+                        forceX = -this.moveForce;
+                    }
+
+                    this._facingRight = false;
+                    this._flip();
+
+                    //move force
+                }
+            
+                this._animator.SetInteger("Anim_state", 1);
             }
-            if (this._move < 0)
+            else
             {
-                this._facingRight = false;
+                this._animator.SetInteger("Anim_state", 0);
             }
-            this._animator.SetInteger("Anim_state", 1);
+
+            if (this._jump > 0)
+            {
+                //jump force
+                if (absVelY < this.velocityRange.maximum)
+                {
+                    forceY = this.jumpForce;
+                }
+            }
         }
         else
-        {
-            this._animator.SetInteger("Anim_state", 0);
-        }
-
-        if (this._jump > 0)
         {
             //call the jump animation
             this._animator.SetInteger("Anim_state", 2);
         }
 
-        this._flip();
+        //Debug.Log(forceX);
+        //Apply forces to the player
+        this._rigidBody2d.AddForce(new Vector2(forceX, forceY));
     }
 
     private void _flip()
